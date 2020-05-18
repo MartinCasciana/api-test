b67590963b96fa5485cd425ccbdcbabf17cc2ec9
martin casciana <mdcasciana@indec.gob.ar>
2020-05-18 19:59:22 -0300

    feat(project): finish instrument and animal

--- src/openapi/api/animal.js
+++ src/openapi/api/animal.js
@@ -6,14 +6,36 @@ module.exports = {
             parameters: [
                 {
                     in: 'query',
-                    name: 'code',
+                    name: 'animal',
                     schema: {
                         type: 'string',
                         pattern: '^[A-Z]{2}$'
                     },
-                    description: 'Código del animal solicitado'
+                    description: 'Especie de animal solicitada'
+                },
+                {
+                    in: 'query',
+                    name: 'scientific_name',
+                    schema: {
+                        type: 'string',
+                        pattern: '^[A-Z]{2}$'
+                    },
+                    description: 'Nombre científico del animal solicitado'
+                },
+                {
+                    in: 'query',
+                    name: 'gender',
+                    schema: { type: 'string' },
+                    description: 'Género del animal solicitado'
+                },
+                {
+                    in: 'params',
+                    name: 'id',
+                    schema: { type: 'uuid' },
+                    description: 'Id del automóvil solicitado'
                 }
             ],
+
             responses: {
                 200: {
                     description: 'list of animals',
@@ -38,6 +60,115 @@ module.exports = {
                 default: {
                     description: 'Error',
                     content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+            }
+        },
+        post: {
+            security: [],
+            requestBody: {
+                description: 'Nuevo Animal',
+                required: true,
+                content: { 'application/json': { schema: { $ref: '#/components/schemas/Animals' } } }
+            },
+            responses: {
+                200: {
+                    description: 'list of animals',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: { 'application/json': { schema: { $ref: '#/components/schemas/Error' } } }
+                }
+            }
+        }
+    },
+    '/api/animal/{id}': {
+        get: {
+            security: [],
+            summary: 'List Animals for ID',
+            parameters: [{$ref: '#/components/parameters/Path'}],
+            responses: {
+                200: {
+                    description: 'ID del Animal',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: { 'application/json': { schema: { $ref: '#/components/schemas/Error' } } }
+                }
+            }
+        },
+        put: {
+            security: [],
+            parameters: [{ $ref: '#/components/parameters/Path' }],
+            requestBody: {
+                description: 'Descripción Opcional',
+                required: true,
+                content: { 'application/json': { schema: { $ref: '#/components/schemas/Animals' } } }
+            },
+            responses: {
+                200: {
+                    description: 'list of animals',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: { 'application/json': { schema: { $ref: '#/components/schemas/Error' } } }
+                }
+            }
+        },
+        delete: {
+            security: [],
+            parameters: [
+                {
+                    in: 'path',
+                    name: 'id',
+                    schema: {
+                        type: 'string',
+                        format: 'uuid'
+                    },
+                    required: true,
+                    description: 'ID del animal solicitado'
+                }
+            ],
+            responses: {
+                200: {
+                    description: 'list of animals',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: { 'application/json': { schema: { $ref: '#/components/schemas/Error' } } }
                 }
             }
         }
--- src/openapi/api/instrument.js
+++ src/openapi/api/instrument.js
@@ -6,14 +6,36 @@ module.exports = {
             parameters: [
                 {
                     in: 'query',
-                    name: 'code',
+                    name: 'hexcode',
                     schema: {
                         type: 'string',
                         pattern: '^[A-Z]{2}$'
                     },
-                    description: 'Código del instrumento solicitado'
+                    description: 'Código Hex del instrumento solicitado'
+                },
+                {
+                    in: 'query',
+                    name: 'family',
+                    schema: {
+                        type: 'string',
+                        pattern: '^[A-Z]{2}$'
+                    },
+                    description: 'Tipo de familia del instrumento solicitado'
+                },
+                {
+                    in: 'query',
+                    name: 'instrument',
+                    schema: { type: 'string' },
+                    description: 'Nombre del instrumento solicitado'
+                },
+                {
+                    in: 'params',
+                    name: 'id',
+                    schema: { type: 'uuid' },
+                    description: 'Id del instrumento solicitado'
                 }
             ],
+
             responses: {
                 200: {
                     description: 'list of instruments',
@@ -38,6 +60,115 @@ module.exports = {
                 default: {
                     description: 'Error',
                     content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+            }
+        },
+        post: {
+            security: [],
+            requestBody: {
+                description: 'Nuevo Instrumento',
+                required: true,
+                content: { 'application/json': { schema: { $ref: '#/components/schemas/Instruments' } } }
+            },
+            responses: {
+                200: {
+                    description: 'list of instruments',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: { 'application/json': { schema: { $ref: '#/components/schemas/Error' } } }
+                }
+            }
+        }
+    },
+    '/api/instrument/{id}': {
+        get: {
+            security: [],
+            summary: 'List Instruments for ID',
+            parameters: [{$ref: '#/components/parameters/Path'}],
+            responses: {
+                200: {
+                    description: 'ID del Instrumento',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: { 'application/json': { schema: { $ref: '#/components/schemas/Error' } } }
+                }
+            }
+        },
+        put: {
+            security: [],
+            parameters: [{ $ref: '#/components/parameters/Path' }],
+            requestBody: {
+                description: 'Descripción Opcional',
+                required: true,
+                content: { 'application/json': { schema: { $ref: '#/components/schemas/Instruments' } } }
+            },
+            responses: {
+                200: {
+                    description: 'list of instruments',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: { 'application/json': { schema: { $ref: '#/components/schemas/Error' } } }
+                }
+            }
+        },
+        delete: {
+            security: [],
+            parameters: [
+                {
+                    in: 'path',
+                    name: 'id',
+                    schema: {
+                        type: 'string',
+                        format: 'uuid'
+                    },
+                    required: true,
+                    description: 'ID del instrumento solicitado'
+                }
+            ],
+            responses: {
+                200: {
+                    description: 'list of instruments',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {}
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: { 'application/json': { schema: { $ref: '#/components/schemas/Error' } } }
                 }
             }
         }
